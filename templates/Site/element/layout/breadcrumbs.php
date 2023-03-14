b373dcf2514a3da9433bdfcf745b1cad3566b64e
a-aoyagi <52394869+a-aoyagi@users.noreply.github.com>
2023-03-14 19:05:53 +0900

    Add breadcrumbs element

---
+++ templates/Site/element/layout/breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var \EntreeCore\View\AppView $this
+ */
+?>
+<div class="breadcrumbs mb-4">
+  <div class="container-xxl small">
+    <?= $this->Breadcrumbs->render() ?>
+  </div>
+</div>
