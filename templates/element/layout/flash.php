6ec95bf8df97341d18597ec4b2108519e7bfa96e
a-aoyagi <52394869+a-aoyagi@users.noreply.github.com>
2023-08-06 08:29:49 +0900

    Add class attribute of the container setting to the flash element

--- templates/element/layout/flash.php
+++ templates/element/layout/flash.php
@@ -2,16 +2,24 @@
 /**
  * Flash message box
  *
+ * @var ?array $container
  * @var ?array $options
  * @var ?string $key
  * @var Cake\View\View $this
  */
 $key = $key ?? 'flash';
 $options = $options ?? [];
+
+$containerClass = $container['class'] ?? null;
+if (is_string($containerClass)) {
+  $containerClass = ' ' . trim($containerClass);
+} else {
+  $containerClass = '';
+}
 ?>
 <?php $message = $this->Flash->render($key, $options); ?>
 <?php if ($message): ?>
-  <div class="container-xxl">
+  <div class="container-xxl<?= h($containerClass) ?>">
     <?= $message ?>
   </div>
 <?php endif ?>
